efa8a15e3cf05b0c727fb8b5857c66d9bbafa69b
RynhAleh <workinplus@yandex.ru>
2022-08-28 09:48:02 +0300

    Добавили бессмысленные строки

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def _roll_dice():
     while True:
         val = [random.randint(1, 6), random.randint(1, 6), random.randint(1, 6), random.randint(1, 6)]
         i += 1
+        i -= 1
+        i += 1
         print('%3i:' % i, val[0], val[1], val[2], val[3])
         if val[0] == val[1] == val[2] == val[3]:
             return 'Вы выкинули %i/%i/%i/%i с %i-й попытки.' % (val[0], val[1], val[2], val[3], i)
